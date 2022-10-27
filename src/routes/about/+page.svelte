f6fdb60fe3fcced1d06a02b5783a107033e4176e
Rob Vandelinder <robjvan@gmail.com>
2022-10-27 11:43:16 -0230

    Added about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>This is a really cool SvelteKit demo that uses the Pokemon API to build a Pokedex</p>
+
+<svelte:head>
+	<title>About Svelte Kit Pokedex</title>
+</svelte:head>
