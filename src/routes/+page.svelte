2742ab0c1471f3e11fca28d79ed122d5ab072292
Rob Vandelinder <robjvan@gmail.com>
2022-10-27 11:44:16 -0230

    Added homepage layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { pokemon } from '../stores/pokestore';
+	import PokemanCard from '../components/pokemanCard.svelte';
+
+	let searchTerm = '';
+	/**
+	 * @type {any[]}
+	 */
+	let filteredPokemon = [];
+
+	$: {
+		if (searchTerm) {
+			filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+		} else {
+			filteredPokemon = [ ...$pokemon ]
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
+
+<input
+	type="text"
+	placeholder="Search Pokemon"
+	class="w-full rounded-md text-lg p-4 border-2 border-grey-200"
+	bind:value={searchTerm}
+/>
+
+<div class="grid gap-4 lg:grid-cols-3 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 py-4">
+	{#each filteredPokemon as pokeman}
+		<PokemanCard {pokeman} />
+	{/each}
+</div>
