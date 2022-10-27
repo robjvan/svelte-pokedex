0ed04cddd0c99b0ca4c5e250f18a25c30ea26d00
Rob Vandelinder <robjvan@gmail.com>
2022-10-27 11:43:44 -0230

    Added pokemon info page

---
+++ src/routes/pokemon/[id]/+page.svelte
@@ -0,0 +1,10 @@
+
+<script>
+  /** @type {import('./$types').PageData} */
+  export let data;
+  const pokeman = data.props.pokeman;
+  console.log(pokeman);
+</script>
+
+<h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
+<img class="h-80 w-80 mx-auto center" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeman.id}.png`} alt={pokeman.name} /> 
