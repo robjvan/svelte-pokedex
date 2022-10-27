37dd8877bf4076537612245b100b512c06a178a3
Rob Vandelinder <robjvan@gmail.com>
2022-10-27 11:44:42 -0230

    Created reusable card

---
+++ src/components/pokemanCard.svelte
@@ -0,0 +1,12 @@
+<script lang=ts>
+	import type { Pokeman } from 'src/interfaces/pokeman.interface';
+
+	export let pokeman: Pokeman; // expect to receive a pokeman param
+</script>
+
+<a
+	class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href={`/pokemon/${pokeman.id}`}
+>
+	<img class="h-40 w-40" src={pokeman.image} alt={pokeman.name} />
+	<h2 class="uppercase text-2xl">{pokeman.id}. {pokeman.name}</h2>
+</a>
