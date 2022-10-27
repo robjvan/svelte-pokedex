5e6b584760d041102d7361f1f76f2c7fef8ca866
Rob Vandelinder <robjvan@gmail.com>
2022-10-27 11:44:04 -0230

    Added load function

---
+++ src/routes/pokemon/[id]/+page.ts
@@ -0,0 +1,10 @@
+export async function load({ params }) {
+	//make api call for more info based on passed if
+	const id = params.id;
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const res = await fetch(url);
+	const pokeman = await res.json();
+	return { 
+    props: { pokeman: pokeman } 
+  };
+}
