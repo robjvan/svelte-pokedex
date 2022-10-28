25f4e2497889f22b817b82991d0a5ec08703b7f6
Rob Vandelinder <robjvan@gmail.com>
2022-10-28 15:36:23 -0230

    Added fields

--- src/interfaces/pokeman.interface.ts
+++ src/interfaces/pokeman.interface.ts
@@ -2,4 +2,13 @@ export interface Pokeman {
   id: number;
   name: string;
   image: string;
+
+  hp: number;
+  attack: number;
+  defense: number;
+  specialAttack: number;
+  specialDefense: number;
+  speed: number;
+  
+  types: string[];
 }
