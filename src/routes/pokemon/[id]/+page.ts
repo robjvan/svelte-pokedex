0e957c71051ea10cd132c464d996778bc5fa1bbb
Rob Vandelinder <robjvan@gmail.com>
2022-10-28 15:50:59 -0230

    Fixed fetch reference

--- src/routes/pokemon/[id]/+page.ts
+++ src/routes/pokemon/[id]/+page.ts
@@ -1,4 +1,4 @@
-export async function load({ params }) {
+export async function load({ fetch, params }) {
 	//make api call for more info based on passed if
 	const id = params.id;
 	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
