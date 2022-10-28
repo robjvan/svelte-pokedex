ed8eb0906c0199aff4bdc2cde6b6f01f24a2822f
Rob Vandelinder <robjvan@gmail.com>
2022-10-28 15:36:36 -0230

    Centered text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
-<p>This is a really cool SvelteKit demo that uses the Pokemon API to build a Pokedex</p>
+<p class="text-center">This is a SvelteKit demo that uses the Pokemon API to build a Pokedex</p>
 
 <svelte:head>
 	<title>About Svelte Kit Pokedex</title>
